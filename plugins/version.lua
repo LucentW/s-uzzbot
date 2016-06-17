f0da1211d7a7c0179518ca46e0584a3bea7e04a5
LucentW <LucentW@users.noreply.github.com>
2016-06-17 21:04:57 +0200

    Put src link into bot's version plugin

--- plugins/version.lua
+++ plugins/version.lua
@@ -2,7 +2,9 @@ do
 
 function run(msg, matches)
   return str2emoji(':information_source:')..' s-uzzbot/telegram-bot '.. VERSION .. [[ 
-This software and its plugins are under the GNU GPL v2 license.]]
+This software and its plugins are under the GNU GPL v2 license.
+
+Sources are available at https://github.com/LucentW/s-uzzbot/]]
 end
 
 return {
