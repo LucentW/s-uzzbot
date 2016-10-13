83b44f23a66d0fb143f3002c1021cfb9af957339
Ferdinando La Mattina <f@lamattina.me>
2016-10-13 18:45:57 +0200

    NSFW plugin
    Added a nsfw plugin that clears the screen when nsfw content is
    submitted.
    
    Triggers via !nsfw and !clear

---
+++ plugins/nsfw.lua
@@ -0,0 +1,16 @@
+﻿do
+
+function run(msg, matches)
+  local answers = {'.NSFW content above\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n NSFW content above'}
+  return answers[math.random(#answers)]
+end
+
+return {
+  description = "Clear Screen from NSFW",
+  usage = "Use !clear or !nsfw to hide nsfw content",
+  patterns = {"^!clear",
+  "^!nsfw"},
+  run = run
+}
+
+end
