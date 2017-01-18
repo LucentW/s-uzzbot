bbfa0992e8470807260dcd8f048f1a76d231cc89
giuseppe marino <giuseppe.marino1999@gmail.com>
2017-01-18 14:30:28 +0100

    Added support to supergroup

--- plugins/broadcast.lua
+++ plugins/broadcast.lua
@@ -1,11 +1,17 @@
 local function returnids(cb_extra, success, result)
   local receiver = cb_extra.receiver
-  local chat_id = result.peer_id
-  local chatname = result.print_name
   for k,v in pairs(result.members) do
     send_large_msg(v.print_name, text)
   end
   send_large_msg(receiver, 'Message broadcasted succesfully')
+end
+
+local function returnidschan(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  for k,v in pairs(result) do
+    send_large_msg(v.print_name, text)
+  end
+  send_large_msg(receiver, text)
 end
 
 local function run(msg, matches)
@@ -17,7 +23,11 @@ local function run(msg, matches)
     text = 'Message for all member of ' .. string.gsub(msg.to.print_name, '_', ' ') .. ' :'
     text = text .. '\n\n' .. matches[1]
     local chat = get_receiver(msg)
-    chat_info(chat, returnids, {receiver=receiver})
+    if not is_chan_msg(msg) then
+      chat_info(chat, returnids, {receiver=receiver})
+    else
+      channel_get_users(chat, returnidschan, {receiver=receiver})
+    end
   end
 end
 
